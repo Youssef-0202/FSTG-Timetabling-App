6c1d703e05d315c01f92b388237a0198f75c3e29
Youssef_AB <youssefaitbahssine23@gmail.com>
2026-04-15 13:12:26 +0100

    task: improve the ui

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "http://192.168.56.1:8000";
 
 async function apiFetch<T>(path: string, options?: RequestInit): Promise<T> {
   const res = await fetch(`${API_BASE}${path}`, {
